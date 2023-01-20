4352440079b23de2f69db4a74086cb2dc7377e66
Troy Delacruz <troy8076@yahoo.com>
2023-01-19 20:24:14 -0500

    Working on components still

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Dropdown from './components/Dropdown';
 
 const App = () => {
     const [characters, setCharacters] = useState();
